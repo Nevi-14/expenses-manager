2523eb0db10731a4b50fc4f8daf07a366043ee1f
Nelson Morera <workemailnelson@gmail.com>
2023-07-14 12:41:39 -0600

    20230714v1.1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  sliderOpts = {
+    zoom: false,
+    slidesPerView: 4,
+    spaceBetween: 2,
+    centeredSlides: false,
+    // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 5
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+     // when window width is >= 940px
+     940: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
 
+     // when window width is >= 1200px
+     1300: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  },
+  };
   constructor() {}
 
 }
